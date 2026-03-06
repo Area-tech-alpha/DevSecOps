0bc1f9cf04639819d2e625527855294ed2571f56
Balofoide <viniciuschavito@gmail.com>
2026-03-06 11:10:44 -0300

    fix:empty lint config

---
+++ lint/eslint.config.mjs
@@ -0,0 +1,32 @@
+import js from "@eslint/js"
+import tseslint from "typescript-eslint"
+import prettier from "eslint-config-prettier"
+
+export default [
+
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+      }
+    }
+  },
+
+  {
+    ignores: [
+      "dist",
+      "node_modules",
+      "coverage"
+    ]
+  },
+
+  prettier
+]
